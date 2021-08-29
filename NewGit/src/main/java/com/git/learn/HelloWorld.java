9f4ad0b4d669759234d878ba650620e79bb30fc2
Music <Music@LAPTOP-Q0D8EKOD>
2021-08-28 21:51:25 -0700

    commit 4

--- NewGit/src/main/java/com/git/learn/HelloWorld.java
+++ NewGit/src/main/java/com/git/learn/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello ");
 		System.out.println("hello hello");
+		System.out.println("hello hello hello");
 	}
 }
